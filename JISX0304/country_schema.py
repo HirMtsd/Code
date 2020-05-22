fbf38b7c29a366e35485b63e18c0498327b27845
HirMtsd <hi_matsuda@yahoo.co.jp>
2020-05-22 14:26:50 +0900

    schemaチェック用コードの移動

--- JISX0304/country_schema.py
+++ JISX0304/country_schema.py
@@ -1,10 +1,10 @@
 import json
 from jsonschema import validate, ValidationError
 
-with open('JISX0304/Country_schema.json', encoding="utf-8") as file_schema:
+with open('Country_schema.json', encoding="utf-8") as file_schema:
     json_schema = json.load(file_schema)
 
-with open('JISX0304/Country_list.json', encoding="utf-8") as file_json:
+with open('Country_list.json', encoding="utf-8") as file_json:
     json_data = json.load(file_json)
 
 try:
@@ -12,4 +12,4 @@ try:
 except ValidationError as e:
     print(e.message)
 
-print('END')
+print('SCUCCESS')
