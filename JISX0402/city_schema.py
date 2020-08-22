14ce4ae9586d0c145fed9efe54fc39854780293e
HirMtsd <hi_matsuda@yahoo.co.jp>
2020-08-23 00:10:14 +0900

    市区町村コード追加

---
+++ JISX0402/city_schema.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2020 @HirMtsd. All Rights Reserved.
+# coding: utf-8
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+# 2020/08/08
+
+import json
+from jsonschema import validate, ValidationError, FormatChecker
+
+with open('City_schema.json', encoding="utf-8") as file_schema:
+    json_schema = json.load(file_schema)
+
+with open('City_list.json', encoding="utf-8") as file_json:
+    json_data = json.load(file_json)
+
+try:
+    validate(json_data, json_schema, format_checker=FormatChecker())
+except ValidationError as e:
+    print(e.message)
+
+print('CHECK END')
--- README.md
+++ README.md
@@ -4,5 +4,9 @@
 ## 都道府県コード
 都道府県コードJSON (https://qiita.com/HirMtsd/items/5de2ee19e086f07921d2)
 
+## 市区町村コード
+市区町村コードJSON ()
+
 ## 国名コード
 国名コードJSON (https://qiita.com/HirMtsd/items/6532ebfd7c486c3b7b44)
+
