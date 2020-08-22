2fb9947c4c23357df36bb8cf3322a4f0fd228597
HirMtsd <hi_matsuda@yahoo.co.jp>
2020-08-23 00:12:00 +0900

    軽微な修正

--- JISX0304/country_schema.py
+++ JISX0304/country_schema.py
@@ -1,5 +1,11 @@
+# Copyright (C) 2020 @HirMtsd. All Rights Reserved.
+# coding: utf-8
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+# 2020/08/08
+
 import json
-from jsonschema import validate, ValidationError
+from jsonschema import validate, ValidationError, FormatChecker
 
 with open('Country_schema.json', encoding="utf-8") as file_schema:
     json_schema = json.load(file_schema)
@@ -8,8 +14,8 @@ with open('Country_list.json', encoding="utf-8") as file_json:
     json_data = json.load(file_json)
 
 try:
-    validate(json_data, json_schema)
+    validate(json_data, json_schema, format_checker=FormatChecker())
 except ValidationError as e:
     print(e.message)
 
-print('SCUCCESS')
+print('CHECK END')
